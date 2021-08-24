dd1cbf1da402290e04fd23bcb00b05a0ed5b5d23
Yi Hu <yi_hu@alumni.brown.edu>
2021-08-23 20:14:45 -0700

    Debugging: [proxy.h] add gzip feature(wrong request.path in cache for binary file)

--- proxy.h
+++ proxy.h
@@ -5,11 +5,14 @@
 #include "csapp.h"
 #include <string.h>
 #include "sbuf.h"
+#include "gzip.h"
 
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
 #define NTHREAD 8
+#define MAX_HEADERS 20
+#define COMPRESSION_LEVEL Z_DEFAULT_COMPRESSION
 
 typedef struct {
     rio_t request_rio;
@@ -17,7 +20,31 @@ typedef struct {
     char hostname[MAXLINE];
     char path[MAXLINE];
     char port[MAXLINE];
+    int enable_compression;
 } request_t;
+
+typedef struct {
+    size_t num_headers;
+    char headers[MAX_HEADERS][MAXLINE];
+    int length_header_index;
+
+} response_header_t;
+
+
+
+typedef struct {
+    rio_t response_rio;
+    int request_compression;
+    z_stream strm;
+    size_t raw_body_length;
+    size_t compressed_body_length;
+    size_t forwarded_length;
+    char compressed_body[CHUNK];
+    response_header_t headers;
+
+} response_t;
+
+
 
 typedef struct cache_line_t cache_line_t;
 typedef struct cache_line_t{
@@ -43,7 +70,7 @@ typedef struct{
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 static int read_request(int fd_from, request_t * request);
 static void forward_request(int fd_to, request_t * request);
-static int forward_headers(int fd_to, request_t * request);
+static int forward_request_headers(int fd_to, request_t * request);
 static void forward_response(int fd_from, int fd_to, request_t * request);
 void clienterror(int fd, char *cause, char *errnum,
                 char *shortmsg, char *longmsg);
@@ -54,5 +81,8 @@ static void write_cache(request_t * request, char * obj, size_t obj_size);
 static cache_line_t * cache_line_init(size_t size, request_t * request);
 static void cache_line_deinit(cache_line_t * cache_line);
 static void cache_init(cache_t * cache);
+static void forward_and_cache_bytes(char * buffer, size_t size, int fd_to, size_t * size_forwarded, char * cache_local);
+static void forward_and_cache_response_headers(response_t * response, char * cache_local, int fd_to);
+
 
 #endif
